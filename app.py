a57a70e2e345a403d6cfc8bb4cc5ee6cc13949cb
Lucas <lucasderito@hotmail.com>
2023-10-14 16:42:02 -0300

    con streamlit y limpiador de texto

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+import streamlit as st
 import openai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
-
+openai.api_key = ""
 
 def limpiar_texto(texto):
     # Elimina saltos de línea y espacios en blanco adicionales.
@@ -39,17 +39,30 @@ def analizar_sentimientos(texto, max_respuesta_length=100):
 
     return respuesta_formateada
 
-print("ANALISIS DE SENTIMIENTOS EN TEXTOS:\n"
-      "A tener en cuenta :\n"
-      "Si el texto que ingresa no espeficica a que hace referencia (peliculas, personas, articulos, etc.)"
-      " puede que de un analisis inexacto\n"
-      "A textos mejor especificados analisis mas exactos\n")
-texto_a_analizar = input("Ingresa el texto que quiere analizar :")
+# print("ANALISIS DE SENTIMIENTOS EN TEXTOS:\n"
+#       "A tener en cuenta :\n"
+#       "Si el texto que ingresa no espeficica a que hace referencia (peliculas, personas, articulos, etc.)"
+#       " puede que de un analisis inexacto\n"
+#       "A textos mejor especificados analisis mas exactos\n")
+# texto_a_analizar = input("Ingresa el texto que quiere analizar :")
+#
+# sentimiento = analizar_sentimientos(texto_a_analizar)
+#
+# # Reemplazar el punto por "Texto analizado:"
+# #sentimiento = "Texto analizado:" + sentimiento
+#
+# # Imprimir la respuesta en una nueva línea
+# print("\n" + sentimiento)
+# Configurar la página de Streamlit
+st.title("Análisis de Sentimientos en Textos")
+st.write("A tener en cuenta: Si el texto que ingresa no especifica a qué hace referencia (películas, personas, artículos, etc.),"
+         " puede que el análisis sea inexacto."
+         "En textos mejor especificados, el análisis será más preciso.")
 
-sentimiento = analizar_sentimientos(texto_a_analizar)
-
-# Reemplazar el punto por "Texto analizado:"
-#sentimiento = "Texto analizado:" + sentimiento
-
-# Imprimir la respuesta en una nueva línea
-print("\n" + sentimiento)
+# Columna izquierda con el campo de texto
+texto_a_analizar = st.text_area("Ingrese el texto que quiere analizar:")
+if st.button("Analizar Texto"):
+    if texto_a_analizar:
+        st.subheader("Resultado del Análisis:")
+        sentimiento = analizar_sentimientos(texto_a_analizar)
+        st.write(sentimiento)
