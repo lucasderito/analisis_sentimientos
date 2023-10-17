de73646591f9fe802f4568b6ff7076dddaca86d7
Lucas <lucasderito@hotmail.com>
2023-10-17 17:15:01 -0300

    terminado

--- app.py
+++ app.py
@@ -2,22 +2,66 @@ import streamlit as st
 import openai
 import os
 from dotenv import load_dotenv
+import requests
+#load_dotenv()
 
-load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = ""
+st.write("<h1 style='color: #66b3ff;font-size: 36px;'>Esta aplicación funciona con API Key de OpenAI</h1>", unsafe_allow_html=True)
 
+user_input = st.text_input("Ingresa tu API key de OpenAI:")
+
+if st.button("Validar API Key"):
+    #print(f"API key ingresada: {user_input}")  # Imprime el valor de la API key ingresada
+
+    openai.api_key = user_input  # Configura la API key ingresada por el usuario
+    #print(f"API key configurada: {openai.api_key}")  # Imprime el valor de la API key configurada
+
+    try:
+        response = openai.Completion.create(
+            engine="davinci", prompt="This is a test."
+        )
+        #print(f"Respuesta de OpenAI: {response}")  # Imprime la respuesta de OpenAI
+        st.success("API key válida. Puedes utilizar la aplicación.")
+        # Coloca aquí el código de tu aplicación
+    except Exception as e:
+        #print(f"Error al validar la API key: {e}")  # Imprime el error si la API key no es válida
+        st.error("API key no válida. Acceso denegado.")
+
+# Agregar una línea horizontal
+st.write('<hr>', unsafe_allow_html=True)
 def limpiar_texto(texto):
     # Elimina saltos de línea y espacios en blanco adicionales.
     texto_limpio = " ".join(texto.split())
     return texto_limpio
 
+# Diccionario de emociones y palabras clave
+emociones_dict = {
+    "gusto": ["gusto", "encanta","encantan", "me gusta","es un buen","hermosa","hermoso", "satisfacción"],
+    "amor": ["gusto", "encanta", "amor", "amo", "me gusta","enamoramiento"],
+    "interesante": ["interesante", "fascinante", "atractivo", "intrigante"],
+    "enfado": ["enfado", "enojado", "molesto", "rabia" , "basta", "furioso"],
+    "tristeza": ["tristeza", "melancolía", "abatido", "deprimido", "triste"],
+    "felicidad": ["feliz", "alegría", "me gusta" "contento", "entusiasmado", "satisfacción"],
+    "sorpresa": ["sorpresa", "asombro", "increíble", "impactante", "tremendo"],
+    "miedo": ["miedo", "aterrorizado", "asustado", "temor" ,"panico"],
+    "desprecio": ["desprecio", "asco", "repulsión", "detesto"],
+    "divertido": ["divertido","divertirnos", "chiste", "risa", "humor"],
+    "orgullo": ["orgullo", "satisfacción", "elogio", "dignidad"],
+    "culpa": ["culpa", "remordimiento", "arrepentimiento", "penitencia"],
+    "emoción": ["emoción", "emocionado", "sentir", "experiencia"],
+    "confusión": ["confusión", "desconcertado", "perplejo", "incomprensión"],
+    "aburrimiento": ["aburrimiento", "monótono", "tedio", "insípido","aburrido"],
+    "tranquilidad": ["tranquilidad", "serenidad", "paz", "calma"],
+    "indiferencia": ["indiferencia", "apático", "desinterés", "neutral","me da lo mismo", "me da igual"],
+    "esperanza": ["esperanza", "optimismo", "esperanzado"],
+    "inquietud": ["inquietud", "nerviosismo", "ansiedad", "preocupación"],
+    # Agrega más emociones y palabras clave aquí
+}
 
 def analizar_sentimientos(texto, max_respuesta_length=100):
     limpiar_texto(texto)
     prompt = (
-        f"Por favor analiza el sentimiento predominante en el siguiente texto : '{texto}'. Pero quiero que me des una"
-        f"explicacion mas detallada como si fuese un profesional (no más de {max_respuesta_length} tokens),")
+        f"Por favor analiza el sentimiento predominante en el siguiente texto: '{texto}'. El sentimiento es: Pero quiero que me des una"
+        f"explicación más detallada como si fuese un profesional (no más de {max_respuesta_length} tokens),")
 
     respuesta = openai.Completion.create(
         engine="text-davinci-002",
@@ -29,40 +73,67 @@ def analizar_sentimientos(texto, max_respuesta_length=100):
 
     respuesta_formateada = respuesta.choices[0].text.strip()
 
+    # Añadir la detección de emociones utilizando el diccionario
+    emociones = {}  # Un diccionario para almacenar las emociones y sus porcentajes
+    for emocion, palabras_clave in emociones_dict.items():
+        for palabra_clave in palabras_clave:
+            if palabra_clave in respuesta_formateada:
+                emociones[emocion] = respuesta_formateada.count(palabra_clave) / len(respuesta_formateada)
+
+    # Rellenar las emociones faltantes con cero
+    for emocion in emociones_dict.keys():
+        if emocion not in emociones:
+            emociones[emocion] = 0.0
+
     # Realizar un recorte si la respuesta excede la longitud máxima deseada
     if len(respuesta_formateada) > max_respuesta_length:
         respuesta_formateada = " ".join(respuesta_formateada.split()[:max_respuesta_length])
 
     # Divide la respuesta en párrafos de aproximadamente 75 caracteres
-    respuesta_formateada = "\n".join(
+    respuesta_formateada = "".join(
         [respuesta_formateada[i:i + 75] for i in range(0, len(respuesta_formateada), 75)])
 
-    return respuesta_formateada
+    return respuesta_formateada, emociones
 
-# print("ANALISIS DE SENTIMIENTOS EN TEXTOS:\n"
-#       "A tener en cuenta :\n"
-#       "Si el texto que ingresa no espeficica a que hace referencia (peliculas, personas, articulos, etc.)"
-#       " puede que de un analisis inexacto\n"
-#       "A textos mejor especificados analisis mas exactos\n")
-# texto_a_analizar = input("Ingresa el texto que quiere analizar :")
-#
-# sentimiento = analizar_sentimientos(texto_a_analizar)
-#
-# # Reemplazar el punto por "Texto analizado:"
-# #sentimiento = "Texto analizado:" + sentimiento
-#
-# # Imprimir la respuesta en una nueva línea
-# print("\n" + sentimiento)
-# Configurar la página de Streamlit
-st.title("Análisis de Sentimientos en Textos")
-st.write("A tener en cuenta: Si el texto que ingresa no especifica a qué hace referencia (películas, personas, artículos, etc.),"
+#imagen del banner
+imagen = "banner.jpg"
+st.image(imagen)
+# Agregar una línea horizontal
+st.write('<hr>', unsafe_allow_html=True)
+st.write("<h1 style='color: #66b3ff;font-size: 28px;'>Análisis Textual:</h1>", unsafe_allow_html=True)
+
+st.write("A tener en cuenta:")
+st.write("Si el texto que ingresa no especifica a qué hace referencia (películas, personas, artículos, etc.),"
          " puede que el análisis sea inexacto."
          "En textos mejor especificados, el análisis será más preciso.")
 
 # Columna izquierda con el campo de texto
-texto_a_analizar = st.text_area("Ingrese el texto que quiere analizar:")
-if st.button("Analizar Texto"):
-    if texto_a_analizar:
-        st.subheader("Resultado del Análisis:")
-        sentimiento = analizar_sentimientos(texto_a_analizar)
-        st.write(sentimiento)
+with st.container():
+    texto_a_analizar = st.text_area("Ingrese el texto que quiere analizar:")
+    if st.button("Analizar Texto"):
+        # Agregar una línea horizontal
+        st.write('<hr>', unsafe_allow_html=True)
+        if texto_a_analizar:
+            st.subheader("Resultado del Análisis:")
+            resultado_analisis, emociones = analizar_sentimientos(texto_a_analizar)
+            st.write(resultado_analisis)
+            # Agregar una línea horizontal
+            st.write('<hr>', unsafe_allow_html=True)
+            # Mostrar el gráfico de barras con emociones
+            st.subheader("Emociones Detectadas:")
+            st.bar_chart(emociones)
+
+st.write('<hr>', unsafe_allow_html=True)
+st.write("<h1 style='color: #66b3ff;font-size: 28px;'>Análisis Grafico:</h1>", unsafe_allow_html=True)
+
+with st.container():
+    st.write("En cuanto al grafico el resultado puede tener diferentes enfoques, ya que el modelo trabaja "
+             "bajo probabilidades por lo que un mismo texto puede tener diferentes puntos de vista."
+             "Puede buscar variantes con el botón de Analizar Gráfico Nuevamente")
+
+    if st.button("Analizar Gráfico Nuevamente"):
+        # Agregar una línea horizontal
+        st.write('<hr>', unsafe_allow_html=True)
+        resultado_analisis, emociones = analizar_sentimientos(texto_a_analizar)
+        st.subheader("Emociones Detectadas:")
+        st.bar_chart(emociones)
